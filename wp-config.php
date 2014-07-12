07a9af093aeb601fa096c3d9a7ca922c43500628
ducthinhdt <ducthinhdt@gmail.com>
2014-07-12 20:21:01 +0700

    sqs

--- wp-config.php
+++ wp-config.php
@@ -16,12 +16,12 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'vnusaservices');
+define('DB_NAME', 'vnusaservifsefsesfsces');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
-/** MySQL database password */
+/** MySQL databafesfesefsfsfsfse password */
 define('DB_PASSWORD', '');
 
 /** MySQL hostname */
